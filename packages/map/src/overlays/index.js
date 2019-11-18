44e79408587be60db7117883e9dc241acb4da5f7
S. Andrew Sheppard <andrew@wq.io>
2019-11-18 13:21:38 +0900

    split into @wq/map and @wq/leaflet

--- packages/map/src/overlays/index.js
+++
@@ -1,6 +0,0 @@
-import Geojson from './Geojson';
-import EsriDynamic from './EsriDynamic';
-import EsriFeature from './EsriFeature';
-import Wms from './Wms';
-
-export { Geojson, EsriDynamic, EsriFeature, Wms };
