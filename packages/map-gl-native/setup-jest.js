968a94af5f8c89958019ecf8737cb8519091361d
S. Andrew Sheppard <andrew@wq.io>
2023-01-13 15:45:34 +0900

    update tests

--- packages/map-gl-native/setup-jest.js
+++ packages/map-gl-native/setup-jest.js
@@ -3,3 +3,6 @@ import { NativeModules } from 'react-native';
 if (!NativeModules.MGLLogging) {
     NativeModules.MGLLogging = {};
 }
+if (!NativeModules.PlatformLocalStorage) {
+    NativeModules.PlatformLocalStorage = {};
+}
